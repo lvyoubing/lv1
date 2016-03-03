63f0f72aed8e3d1a91926fb6ceb87747f9b582cd
lvyoubing <lvhuanda@admindeiMac-88.local>
2016-03-03 10:54:05 +0800

    A+B

---
+++ main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  lv1
+//
+//  Created by 吕欢达 on 16/3/3.
+//  Copyright © 2016年 吕欢达. All rights reserved.
+//
+
+import Foundation
+
+var a:Int,b:Int,c:Int
+a=100
+b=100
+c=a+b
+print("\(c)")
